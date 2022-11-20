69e23e838be39fb494c06f24c1e495ff39ae0edd
shizopudge <91852426+shizopudge@users.noreply.github.com>
2022-11-20 21:58:50 +0500

    fourth_commit

--- lib/screens/entry_screen.dart
+++ lib/screens/entry_screen.dart
@@ -23,15 +23,71 @@ class _EntryScreenState extends ConsumerState<EntryScreen> {
   void createEntry(Doctor doctor) {
     ref
         .read(entryControllerProvider.notifier)
-        .createEntry(doctor, date, time, dateCellId, context);
+        .createEntry(doctor, date, time, dateCellId.trim(), context);
+  }
+
+  int currentMonthNumber = DateTime.now().month;
+  late String currentMonth;
+  int currentYear = DateTime.now().year;
+  int k = 0;
+
+  getCurrentMounth() {
+    switch (currentMonthNumber) {
+      case 1:
+        currentMonth = 'Январь';
+        break;
+      case 2:
+        currentMonth = 'Февраль';
+        break;
+      case 3:
+        currentMonth = 'Март';
+        break;
+      case 4:
+        currentMonth = 'Апрель';
+        break;
+      case 5:
+        currentMonth = 'Май';
+        break;
+      case 6:
+        currentMonth = 'Июнь';
+        break;
+      case 7:
+        currentMonth = 'Июль';
+        break;
+      case 8:
+        currentMonth = 'Август';
+        break;
+      case 9:
+        currentMonth = 'Сентябрь';
+        break;
+      case 10:
+        currentMonth = 'Октябрь';
+        break;
+      case 11:
+        currentMonth = 'Ноябрь';
+        break;
+      case 12:
+        currentMonth = 'Декабрь';
+        break;
+      default:
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    getCurrentMounth();
+    print(currentMonthNumber);
   }
 
   bool isDatePicked = false;
   bool isDataEntered = false;
-  List<String> times = [];
+  bool isDateCellTapped = false;
+  bool isTimeCellTapped = false;
+  List<DateTime> times = [];
   String dateCellId = '';
-  String date = '';
-  String time = '';
+  late DateTime date;
+  late DateTime time;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -72,104 +128,230 @@ class _EntryScreenState extends ConsumerState<EntryScreen> {
               error: (error, stackTrace) => ErrorText(error: error.toString()),
               loading: () => const Loader())
           : null,
-      body: ref.watch(getEntryCellsProvider(widget.doctorId)).when(
-          data: (entryDateCells) {
-            return Column(
-              children: [
-                const Padding(
-                  padding: EdgeInsets.all(8.0),
-                  child: Text(
-                    'Выберите дату',
-                    textAlign: TextAlign.center,
-                    style: TextStyle(
-                      fontSize: 24,
-                      color: Colors.white,
+      body: ref
+          .watch(getEntryCellsByMonthAndYearProvider(MyParameter2(
+              monthNumber: currentMonthNumber,
+              doctorId: widget.doctorId,
+              year: currentYear)))
+          .when(
+              data: (entryDateCells) {
+                return Column(
+                  children: [
+                    const Padding(
+                      padding: EdgeInsets.all(8.0),
+                      child: Text(
+                        'Выберите дату',
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          fontSize: 24,
+                          color: Colors.white,
+                        ),
+                      ),
                     ),
-                  ),
-                ),
-                Expanded(
-                  child: GridView.builder(
-                      gridDelegate:
-                          const SliverGridDelegateWithFixedCrossAxisCount(
-                              crossAxisCount: 4),
-                      itemCount: entryDateCells.length,
-                      itemBuilder: (context, index) {
-                        final dateCell = entryDateCells[index];
-                        return InkWell(
-                          onTap: dateCell.time.isNotEmpty
-                              ? () {
+                    Padding(
+                      padding: const EdgeInsets.all(2.0),
+                      child: Text(
+                        currentYear.toString(),
+                        textAlign: TextAlign.center,
+                        style: const TextStyle(
+                          fontSize: 22,
+                          color: Colors.white,
+                        ),
+                      ),
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        k == 0
+                            ? const SizedBox()
+                            : IconButton(
+                                onPressed: () {
+                                  if (currentMonthNumber == 1) {
+                                    setState(() {
+                                      currentMonthNumber = 12;
+                                      currentYear--;
+                                    });
+                                  } else {
+                                    setState(() {
+                                      currentMonthNumber =
+                                          currentMonthNumber - 1;
+                                    });
+                                  }
                                   setState(() {
-                                    isDatePicked = true;
-                                    dateCellId = dateCell.id;
-                                    date = dateCell.date;
-                                    times = dateCell.time;
+                                    k--;
+                                    isDatePicked = false;
                                   });
-                                }
-                              : null,
-                          child: Card(
-                            color: dateCell.time.isNotEmpty
-                                ? Colors.blue
-                                : Colors.grey,
-                            child: Center(
-                              child: Text(
-                                dateCell.date,
-                                style: const TextStyle(fontSize: 30),
-                              ),
+                                  print(currentMonthNumber);
+                                  getCurrentMounth();
+                                },
+                                icon: const Icon(
+                                  Icons.arrow_back_ios_outlined,
+                                  size: 26,
+                                  color: Colors.white,
+                                )),
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Text(
+                            currentMonth,
+                            textAlign: TextAlign.center,
+                            style: const TextStyle(
+                              fontSize: 24,
+                              color: Colors.white,
                             ),
-                          ),
-                        );
-                      }),
-                ),
-                isDatePicked
-                    ? const Padding(
-                        padding: EdgeInsets.all(8.0),
-                        child: Text(
-                          'Выберите время',
-                          textAlign: TextAlign.center,
-                          style: TextStyle(
-                            fontSize: 24,
-                            color: Colors.white,
                           ),
                         ),
-                      )
-                    : const SizedBox(),
-                Expanded(
-                  child: GridView.builder(
-                      gridDelegate:
-                          const SliverGridDelegateWithFixedCrossAxisCount(
-                              crossAxisCount: 4),
-                      itemCount: times.length,
-                      itemBuilder: (context, index) {
-                        final timeCell = times[index];
-                        return InkWell(
-                          onTap: timeCell.isNotEmpty
-                              ? () {
+                        k > 0
+                            ? const SizedBox()
+                            : IconButton(
+                                onPressed: () {
+                                  if (currentMonthNumber == 12) {
+                                    setState(() {
+                                      currentMonthNumber = 1;
+                                      currentYear++;
+                                    });
+                                  } else {
+                                    setState(() {
+                                      currentMonthNumber =
+                                          currentMonthNumber + 1;
+                                    });
+                                  }
                                   setState(() {
-                                    time = timeCell;
-                                    isDataEntered = true;
+                                    k++;
+                                    isDatePicked = false;
                                   });
-                                  print(date);
-                                  print(time);
-                                  print(dateCellId);
-                                }
-                              : null,
-                          child: Card(
-                            color: Colors.blue,
-                            child: Center(
-                              child: Text(
-                                timeCell,
-                                style: const TextStyle(fontSize: 30),
+                                  print(currentMonthNumber);
+                                  getCurrentMounth();
+                                },
+                                icon: const Icon(
+                                  Icons.arrow_forward_ios_outlined,
+                                  size: 26,
+                                  color: Colors.white,
+                                )),
+                      ],
+                    ),
+                    // Expanded(
+                    //   child: ListView.builder(
+                    //     itemCount: entryDateCells.length,
+                    //     itemBuilder: (BuildContext context, int index) {
+                    //       final dateCell = entryDateCells[index];
+                    //       if (dateCell.date.month == DateTime.now().month &&
+                    //           dateCell.date.day > DateTime.now().day) {
+                    //         return InkWell(
+                    //           onTap: dateCell.time.isNotEmpty
+                    //               ? () {
+                    //                   setState(() {
+                    //                     isDatePicked = true;
+                    //                     dateCellId = dateCell.id;
+                    //                     date = dateCell.date;
+                    //                     times = dateCell.time;
+                    //                   });
+                    //                   print(dateCellId.trim());
+                    //                 }
+                    //               : null,
+                    //           child: Card(
+                    //             color: dateCell.time.isNotEmpty
+                    //                 ? Colors.blue
+                    //                 : Colors.grey,
+                    //             child: Center(
+                    //               child: Text(
+                    //                 dateCell.date.day.toString(),
+                    //                 style: const TextStyle(fontSize: 30),
+                    //               ),
+                    //             ),
+                    //           ),
+                    //         );
+                    //       }
+                    //       return const SizedBox();
+                    //     },
+                    //   ),
+                    // ),
+                    Expanded(
+                      child: GridView.builder(
+                          gridDelegate:
+                              const SliverGridDelegateWithFixedCrossAxisCount(
+                                  crossAxisCount: 5),
+                          itemCount: entryDateCells.length,
+                          itemBuilder: (context, index) {
+                            final dateCell = entryDateCells[index];
+                            if (dateCell.date.month == currentMonthNumber) {
+                              return InkWell(
+                                onTap: dateCell.time.isNotEmpty
+                                    ? () {
+                                        setState(() {
+                                          isDatePicked = true;
+                                          dateCellId = dateCell.id;
+                                          date = dateCell.date;
+                                          times = dateCell.time;
+                                        });
+                                        print(dateCellId.trim());
+                                      }
+                                    : null,
+                                child: Card(
+                                  color: dateCell.time.isNotEmpty
+                                      ? Colors.blue
+                                      : Colors.grey,
+                                  child: Center(
+                                    child: Text(
+                                      dateCell.date.day.toString(),
+                                      style: const TextStyle(fontSize: 30),
+                                    ),
+                                  ),
+                                ),
+                              );
+                            }
+                            return const SizedBox();
+                          }),
+                    ),
+                    isDatePicked
+                        ? const Padding(
+                            padding: EdgeInsets.all(8.0),
+                            child: Text(
+                              'Выберите время',
+                              textAlign: TextAlign.center,
+                              style: TextStyle(
+                                fontSize: 24,
+                                color: Colors.white,
                               ),
                             ),
-                          ),
-                        );
-                      }),
-                ),
-              ],
-            );
-          },
-          error: (error, stackTrace) => ErrorText(error: error.toString()),
-          loading: () => const Loader()),
+                          )
+                        : const SizedBox(),
+                    isDatePicked
+                        ? Expanded(
+                            child: GridView.builder(
+                                gridDelegate:
+                                    const SliverGridDelegateWithFixedCrossAxisCount(
+                                        crossAxisCount: 5),
+                                itemCount: times.length,
+                                itemBuilder: (context, index) {
+                                  final timeCell = times[index];
+                                  return InkWell(
+                                    onTap: () {
+                                      setState(() {
+                                        time = timeCell;
+                                        isDataEntered = true;
+                                      });
+                                      print(date);
+                                      print(time);
+                                      print(dateCellId);
+                                    },
+                                    child: Card(
+                                      color: Colors.blue,
+                                      child: Center(
+                                        child: Text(
+                                          '${timeCell.hour}:${timeCell.minute}',
+                                          style: const TextStyle(fontSize: 24),
+                                        ),
+                                      ),
+                                    ),
+                                  );
+                                }),
+                          )
+                        : const SizedBox(),
+                  ],
+                );
+              },
+              error: (error, stackTrace) => ErrorText(error: error.toString()),
+              loading: () => const Loader()),
     );
   }
 }
